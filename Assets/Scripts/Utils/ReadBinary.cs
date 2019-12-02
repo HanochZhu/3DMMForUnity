04a796089c58806c3a66aead4247797df848311c
HanochZhu <943264652@qq.com>
2019-12-03 02:01:59 +0900

    init project

---
+++ Assets/Scripts/Utils/ReadBinary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ReadBinary
+{
+    
+    public static byte ReadBinaryFromFile(string filePath)
+    {
+        using (FileStream fs = new FileStream(filePath, FileMode.Open))
+        {
+            
+            BinaryReader binaryReader = new BinaryReader(fs);
+            //binaryReader.
+        }
+
+    }
+}
